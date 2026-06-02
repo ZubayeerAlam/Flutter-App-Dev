379292306abc6808e55f7336768b3c420681e9af
Zubayeer Alam <zubayeeralam@gmail.com>
2026-06-03 03:26:39 +0600

    dark theme

---
+++ prectice/Ostad All Module/Module5-6-7/Habit_traacker/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'dashboard_screen.dart';
+
+//new setup done
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+          colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      scaffoldBackgroundColor: Color(0xFFD3D3D3),
+
+
+      appBarTheme: AppBarTheme(
+        backgroundColor: Color(0xFFD3D3D3),
+        foregroundColor: Colors.black,
+        elevation: 1,
+        centerTitle: false,
+      ),
+
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+    shape: CircleBorder(),
+
+        ),
+      ),
+
+        darkTheme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Color(0xFF121212),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color(0xFF121212),
+            foregroundColor: Colors.white,
+            elevation: 1,
+            centerTitle: false,
+          ),
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            shape: CircleBorder(),
+          ),
+        ),
+      home: const DashboardScreen(),
+    );
+  }
+}
+
