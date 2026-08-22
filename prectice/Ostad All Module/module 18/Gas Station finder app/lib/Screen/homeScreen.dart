dabcdf6e5eade3ca7c414a03336761da61d2163f
Zubayeer Alam <zubayeeralam@gmail.com>
2026-08-22 22:34:59 +0600

    marker add, permission Enable , Camera pos update

--- prectice/Ostad All Module/module 18/Gas Station finder app/lib/Screen/homeScreen.dart
+++ prectice/Ostad All Module/module 18/Gas Station finder app/lib/Screen/homeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
 
@@ -9,7 +11,33 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
-  final LatLng _center= const LatLng(23.810331, 90.412521);
+  LatLng _center = const LatLng(45.033333, 31.233334);
+
+  Future<Position> _getCurrentLocation() async{
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return false;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return false;
+    }
+    return true;
+  }
+
+  GoogleMapController? mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +45,36 @@ class _homeScreenState extends State<homeScreen> {
       appBar: AppBar(
         title: Text("Gas Station Finder"),
       ),
-      body: GoogleMap(initialCameraPosition: CameraPosition(target: _center,zoom: 11.0)),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        markers:{
+          Marker(markerId: MarkerId("me"), position: _center)
+        }
+
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     bool permissionGranted = await _checkLocationPermission();
+      //     if (permissionGranted) {
+      //       Position position = await _getCurrentLocation();
+      //       setState(() {
+      //         _center = LatLng(position.latitude, position.longitude);
+      //       });
+      //
+      //       mapController?.animateCamera(CameraUpdate.newLatLngZoom(_center, 11.0));
+      //
+      //     }
+      //     else{
+      //       print("Location permission denied");
+      //     }
+      //   },
+      //   child: Icon(Icons.location_on),
+      // ),
     );
   }
 }
